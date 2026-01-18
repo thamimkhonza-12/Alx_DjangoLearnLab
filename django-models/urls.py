ef81039a4a912fa4642e54e723737073c598bbd6
thamimkhonza-12 <thamimkhonza@gmail.com>
2026-01-18 15:33:30 +0200

    updated urls

--- django-models/urls.py
+++ django-models/urls.py
@@ -37,3 +37,15 @@ urlpatterns = [
     path('', include('relationship_app.urls')),
 ]
 
+from django.urls import path
+from .views import add_book, edit_book, delete_book, user_login, user_logout, register
+
+urlpatterns = [
+    path('add_book/', add_book, name='add_book'),
+    path('edit_book/<int:book_id>/', edit_book, name='edit_book'),
+    path('delete_book/<int:book_id>/', delete_book, name='delete_book'),
+    path('login/', user_login, name='login'),
+    path('logout/', user_logout, name='logout'),
+    path('register/', register, name='register'),
+]
+
