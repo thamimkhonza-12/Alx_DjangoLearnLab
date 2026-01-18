7773ca6940c35893ccf9fe564d149402e7fa1248
thamimkhonza-12 <thamimkhonza@gmail.com>
2026-01-18 15:40:23 +0200

    urls

--- django-models/urls.py
+++ django-models/urls.py
@@ -1,44 +1,8 @@
-"""
-URL configuration for LibraryProject project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
-
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('relationship_app.urls')),  # Include relationship_app URLs
-]
-
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('relationship_app.urls')),
-]
-
-from django.urls import path
-from .views import add_book, edit_book, delete_book, user_login, user_logout, register
+from .views import (
+    add_book, edit_book, delete_book,
+    user_login, user_logout, register
+)
 
 urlpatterns = [
     path('add_book/', add_book, name='add_book'),
@@ -48,4 +12,3 @@ urlpatterns = [
     path('logout/', user_logout, name='logout'),
     path('register/', register, name='register'),
 ]
-
