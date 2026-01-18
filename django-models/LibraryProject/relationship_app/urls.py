a8b9d3e952ed50dd2c78d8d00df66953520780ce
thamimkhonza-12 <thamimkhonza@gmail.com>
2026-01-18 15:47:28 +0200

    views colllaboration

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,88 +1,13 @@
-"""
-URL configuration for LibraryProject project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path
-from django.contrib import admin
-from django.urls import path, include
-
+from .views import (
+    add_book, edit_book, delete_book,
+    user_login, user_logout, register
+)
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
-
-
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('relationship_app.urls')),  # Include relationship_app URLs
-]
-
-from django.urls import path
-from .views import book_list, LibraryDetailView
-
-urlpatterns = [
-    path("books/", book_list, name="book-list"),
-    path("libraries/<int:pk>/", LibraryDetailView.as_view(), name="library-detail"),
-]
-
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("relationship_app.urls")),
-]
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('books/', views.book_list_view, name='book-list'),
-]
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('books/', views.book_list_view, name='book-list'),
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library-detail'),
-]
-
-from django.urls import path
-from .views import CustomLoginView, CustomLogoutView, register
-
-urlpatterns = [
-    path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', CustomLogoutView.as_view(), name='logout'),
-    path('register/', register, name='register'),
-]
-from django.urls import path
-from .views import user_login, user_logout, register
-
-urlpatterns = [
-    path('login/', user_login, name='login'),
-    path('logout/', user_logout, name='logout'),
-    path('register/', register, name='register'),
-]
-
-from django.urls import path
-from .views import add_book, edit_book, delete_book
-
-urlpatterns = [
-    path('books/add/', add_book, name='add_book'),
-    path('books/edit/<int:book_id>/', edit_book, name='edit_book'),
-    path('books/delete/<int:book_id>/', delete_book, name='delete_book'),
+    path("add_book/", add_book, name="add_book"),
+    path("edit_book/<int:book_id>/", edit_book, name="edit_book"),
+    path("delete_book/<int:book_id>/", delete_book, name="delete_book"),
+    path("login/", user_login, name="login"),
+    path("logout/", user_logout, name="logout"),
+    path("register/", register, name="register"),
 ]
