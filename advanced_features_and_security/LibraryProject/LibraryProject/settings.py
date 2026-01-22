225097b19efef71d8e0c97ebc709f66f9153c74c
thamimkhonza-12 <thamimkhonza@gmail.com>
2026-01-22 19:45:13 +0200

    settings add-ons

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -49,3 +49,15 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'LibraryProject.wsgi.application'
+
+
+# ===============================
+# Security Settings
+# ===============================
+
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+X_FRAME_OPTIONS = 'DENY'
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
