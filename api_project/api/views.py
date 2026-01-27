878d25dacfe1c1471cb8d19a279a9f3c76f7eac2
thamimkhonza-12 <thamimkhonza@gmail.com>
2026-01-27 19:57:00 +0200

    api project

---
+++ api_project/api/views.py
@@ -0,0 +1,7 @@
+from rest_framework.generics import ListAPIView
+from .models import Book
+from .serializers import BookSerializer
+
+class BookList(ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
