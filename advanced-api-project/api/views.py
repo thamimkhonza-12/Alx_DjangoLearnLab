30a8e4b6c54809a05b31e24b867973029c39a8dd
thamimkhonza-12 <thamimkhonza@gmail.com>
2026-02-04 19:00:02 +0200

    filters

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,9 @@
+
 from rest_framework import generics, permissions
 from .models import Book
 from .serializers import BookSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 
