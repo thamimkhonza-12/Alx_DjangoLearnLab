9d17316838cdc35ab88e8e000a0d636f2c2c6bc3
thamimkhonza-12 <thamimkhonza@gmail.com>
2026-02-10 19:07:25 +0200

    urls refactor

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -12,6 +12,11 @@ urlpatterns = [
 from django.urls import path
 from . import views
 from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
     CommentCreateView,
     CommentUpdateView,
     CommentDeleteView,
@@ -19,8 +24,11 @@ from .views import (
 
 urlpatterns = [
     # POSTS
-    path('', views.PostListView.as_view(), name='post-list'),
-    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('', PostListView.as_view(), name='post-list'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 
     # COMMENTS
     path(
